8a435259a6374ce666da264bca389e2c09f5fb6b
Rachel Lyons <rlyons21@hotmail.com>
2016-06-24 11:12:47 -0500

    updated RPS choose page

--- RPS/choose.php
+++ RPS/choose.php
@@ -10,7 +10,7 @@
 	<h3> Do you want to choose Rock, Paper or Scissors?</h3>
 		<form>
   			Choice:<br>
-  			<input type="text" name="choice"><br>
+  			<input type="text" name="userchoice"><br>
     	</form>
 
 
@@ -18,7 +18,19 @@
     		$choices = array("Rock", "Paper", "Scissors");
 			$comp_choice = $choices[rand(0,count($choices)-1)];
 
-    	?>
+			$user_choice = $_GET["userchoice"];
+
+			if($user_choice == $comp_choice){ ?>
+				<p> You and the computer both chose <?php $user_choice ?>. Choose again!</p>
+			<?php } ?>
+
+			<?php
+			else{
+				
+			}
+    
+
+
 
 
 
