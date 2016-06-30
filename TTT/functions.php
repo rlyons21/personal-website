a5d77f975197ac1f1f025a4a58d3fa23f293e7bf
Rachel Lyons <rlyons21@hotmail.com>
2016-06-29 20:49:15 -0500

    built TTT everything works

---
+++ TTT/functions.php
@@ -0,0 +1,109 @@
+<?php 
+# Determines if a square should be empty, X or O
+
+# $num represents a square on the board, it will equal 0,1 or
+#2. It comes from $grid
+
+# Returns what mark, if any, to place in a square
+
+  function whatMark ($num) {
+    switch ($num) {
+      case 1:
+        return "X";
+        break;
+      case 2:
+        return "O";
+        break;
+      default:
+        return "";
+        break;
+    }
+  }
+
+# Determine if a player can mark a given square
+
+# $grid is a 9-digit string that represents each square on the 
+#board. $square is a number 0-8 that represents a square.
+#$winner a varriable that is not set until X or O wins (it
+#will be either X or O).
+
+# returns true if a player can mark the square, returns false
+#if they cant
+  function isItEmpty($grid, $square, $winner) {
+    return $grid[$square] == 0 && !isset($winner);
+  }
+
+# Prints each square of the grid and switches players
+
+#$grid is a 9-digit string that represents each square on the
+#board(0=empty, 1= x, 2= O). $square is a number 0-8 that
+#represents a square. $player represents whos turn it is
+#$winner a varriable that is not set until X or O wins
+
+#Creates a square with its contents using HTML
+
+  function printSquare($grid, $square, $player, $winner) {
+    $mark = whatMark($grid[$square]);
+
+    if (isItEmpty($grid, $square, $winner)) {
+    
+      $nextPlayer = ($player == 1) ? 2 : 1;
+      $newGrid = substr_replace($grid, $player, $square, 1);
+
+      echo <<<HTML
+        <a class="square" href="?grid=$newGrid&player=$nextPlayer">$mark</a>
+HTML;
+    } else {
+      echo <<<HTML
+        <div class="square">$mark</div>   
+HTML;
+    }
+  }
+
+
+#prints the entire grid
+
+#$grid is a 9-digit string that represents each square on the
+#board(0=empty, 1= x, 2= O). $player represents whos turn it is
+#$winner a varriable that is not set until X or O wins
+
+##Creates the grid with all of the squares using HTML
+
+  function printGrid($grid, $player, $winner) {
+    for ($i = 0; $i < 9; $i++) {
+      printSquare($grid, $i, $player, $winner);
+    }
+  }
+
+# Checks for a winner or draw
+
+# grid is a 9-digit string that represents each square on the
+# board(0=empty, 1= x, 2= O).
+
+# Returns "its a draw!!", "x wins!!", "o wins!!", or nothing 
+# to $winner.
+  function checkWinner($grid) {
+    if(strpos($grid,"0")===false){
+      return "It's a draw!!";
+    }
+    $wins = [];
+
+    $wins[0] = $grid[0] . $grid[1] . $grid[2];
+    $wins[1] = $grid[3] . $grid[4] . $grid[5];
+    $wins[2] = $grid[6] . $grid[7] . $grid[8];
+    $wins[3] = $grid[0] . $grid[3] . $grid[6];
+    $wins[4] = $grid[1] . $grid[4] . $grid[7];
+    $wins[5] = $grid[2] . $grid[5] . $grid[8];
+    $wins[6] = $grid[0] . $grid[4] . $grid[8];
+    $wins[7] = $grid[2] . $grid[4] . $grid[6];
+
+    foreach ($wins as $way_to_win) {
+      if ($way_to_win == 111) {
+        return "X Wins!!";
+      }
+      elseif($way_to_win == 222){
+        return "O Wins!!";
+      }
+    }
+  }
+?>
--- TTT/index2.php
+++ TTT/index2.php
@@ -1,13 +1,35 @@
-<?php $title = "Tic Tac Toe" ?>
 <?php $pageName = "index2.php" ?>
-<?php include("../top.php") ?>
-<?php include("../navigation.php") ?>
+<?php include 'functions.php' ?>
+<?php include '../navigation.php' ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Tic Tac Toe</title>
+  <link rel="stylesheet" href = "../stylesheet.css">
+  <link rel="stylesheet" href="TTTstylesheet.css">
+</head>
+<body class = TTTcontent>
 
-<body class=TTTcontent>
+<div class="grid">
+<?php
+  parse_str($_SERVER['QUERY_STRING'], $query);
+  $grid = isset($query['grid']) ? $query['grid'] : "000000000";
+  $player = isset($query['player']) ? $query['player'] : 1;
+  $winner = checkWinner($grid);
+  printGrid($grid, $player, $winner);
+?></div>
 
-<form action="TTTgame.php">
-  		Player 1 Name:<input type="text" name="p1Name"><br>
-		<br>
-		Player 2 Name:<input type="text" name="p2Name"><br>
-  		<input type="submit" value="Let's Play!">
-</form>
+<?php
+  
+  if ($winner) {
+    echo <<<HTML
+      <div class="winner">$winner</div>
+HTML;
+  }
+?>
+
+  <a href="index.php" class="reset">Reset</a>
+
+</body>
+</html>
